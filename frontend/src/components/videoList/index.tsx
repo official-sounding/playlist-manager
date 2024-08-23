c60c1853d86d4c154349d96091e3edd617845782
Peter Elliott <pelliott@official-sounding.com>
2024-08-22 22:08:57 -0500

    table searching & styling

--- frontend/src/components/videoList/index.tsx
+++ frontend/src/components/videoList/index.tsx
@@ -2,35 +2,63 @@ import { Download } from '../download';
 
 import classes from './styles.module.css';
 import { TagList } from './tagList';
-import { useAppSelector } from '../../store';
+import { useAppDispatch, useAppSelector } from '../../store';
+import { useMemo, useState } from 'react';
+import { updateShowThumbnails } from '../../store/slices/config';
 
 export function VideoList() {
+    const dispatch = useAppDispatch();
+
     const { showThumbnails } = useAppSelector((state) => state.config);
-    const videos = useAppSelector((state) => state.video.allVideos);
+    const allVideos = useAppSelector((state) => state.video.allVideos);
+    const [search, setSearch] = useState<string>('');
+
+    const videos = useMemo(() => {
+        const searchValue = search.trim();
+        if(!searchValue) {
+            return allVideos;
+        }
+
+        const searchRegex = new RegExp(searchValue, 'i');
+
+        return allVideos.filter(v => v.filename.search(searchRegex) >= 0 || v.title.search(searchRegex) >= 0 || v.tags.some(t => t.title.search(searchRegex) >= 0))
+    }, [search, allVideos]);
 
     return (
         <>
             <Download />
-            <div>{videos.length} Videos</div>
+            <hr />
+            <div className={classes.searchbar}>
+                <input value={search} placeholder='Type here to search' onChange={e => setSearch(e.target.value)} className={classes.searchbox} />
+                <div className={classes.videocount}>{videos.length} Videos {search.length > 0 && `${allVideos.length} total`}</div>
+            </div>
+            <div>
+                <label>
+                    <input type="checkbox" checked={showThumbnails} onChange={(e) => dispatch(updateShowThumbnails(e.target.checked))} />
+                    Show Thumbnails
+                </label>
+            </div>
+            
+            
             <table className={classes.videoList}>
                 <thead>
                     <tr>
-                        <th>Thumbnail</th>
+                        { showThumbnails && <th className={classes.thumbnail}>Thumbnail</th> }
                         <th>Title</th>
-                        <th>Details</th>
+                        <th className={classes.details}>Details</th>
                         <th>Tags</th>
                     </tr>
                 </thead>
                 <tbody>
                     {videos.map((v) => (
                         <tr key={v.id}>
-                            <td>
-                                { showThumbnails && <img src={v.thumbnailUrl} className={classes.thumbnail} /> }
-                            </td>
+                            { showThumbnails && <td>
+                             <img src={v.thumbnailUrl} className={classes.thumbnail} />
+                            </td>}
                             <td className={classes.videoDetails}>
                                 <a href={v.videoUrl} target='_blank'>{v.title}</a>
                             </td>
-                            <td className={classes.secondary}>{v.duration} seconds</td>
+                            <td className={classes.secondary}>{v.duration} sec</td>
                             <td className={classes.secondary}>
                                 <TagList video={v} />
                             </td>
